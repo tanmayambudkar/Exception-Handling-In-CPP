91eab71cd4d583c11866cd433fabeb38d0e801d3
Tanmay Ambudkar <tanmayambudkar@gmail.com>
2025-09-30 23:13:16 +0530

    Add files via upload

---
+++ Age Exception Checker.cpp
@@ -0,0 +1,20 @@
+//Tanmay Ambudkar
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age;
+    cout<<"Enter age: ";
+    cin>>age;
+    try{
+        if(age<18){
+            throw age;
+        }
+        else{
+            cout<<"Age: "<<age<<"\nAPPROVED"<<endl;
+        }
+    }
+    catch(int a){
+        cout<<"\nERROR: Underage! ("<<age<<")"<<endl;
+    }
+}
